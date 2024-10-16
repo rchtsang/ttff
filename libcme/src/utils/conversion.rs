be76ebc8a6c649323376f3f001fdf8bc4477d53a
rchtsang <rchtsang@ucdavis.edu>
2024-10-15 19:50:43 -0700

    add an unsafe bytes to u32 ref cast for potential speed gains later before i forget transmute exists. don't use it for now

--- libcme/src/utils/conversion.rs
+++ libcme/src/utils/conversion.rs
@@ -9,3 +9,7 @@ pub fn bytes_as_u32_le(src: &[u8]) -> u32 {
 pub fn bytes_as_u32_be(src: &[u8]) -> u32 {
     u32::from_be_bytes(src.try_into().unwrap())
 }
+
+pub unsafe fn cast_as_u32_ref(src: &[u8]) -> &u32 {
+    std::mem::transmute::<&[u8; 4], &u32>(src.try_into().unwrap())
+}
