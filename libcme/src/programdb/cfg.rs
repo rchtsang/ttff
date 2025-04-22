388fb2b56105474934f41b67060212e3f229dc45
rchtsang <rchtsang@ucdavis.edu>
2025-04-21 21:00:10 -0700

    add CFGraph and Block types to programdb

---
+++ libcme/src/programdb/cfg.rs
@@ -0,0 +1,122 @@
+//! cfg.rs
+//! 
+//! control flow graph implementation
+//! 
+//! the CFGraph doesn't store actual instructions, just the addresses.
+//! instructions should be fetched from the programdb's cache.
+use std::fmt;
+use std::ops::Range;
+use std::hash::BuildHasherDefault;
+// use itertools::Itertools;
+use thiserror::Error;
+use ahash;
+use nohash::IntMap;
+use iset::IntervalMap;
+use petgraph::{
+    Directed,
+    graphmap::GraphMap,
+};
+
+pub use fugue_core::prelude::*;
+
+use crate::types::*;
+use crate::utils::*;
+
+use super::types::*;
+
+type AHashState = BuildHasherDefault<ahash::AHasher>;
+
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("block @ {0:#x} already translated")]
+    BlockAlreadyExists(u64),
+    #[error("block @ {0:#x} does not exist")]
+    BlockDoesNotExist(u64),
+}
+
+/// control flow graph
+/// 
+/// by default, is not normalized. must be normalized before analysis
+pub struct CFGraph<'arena> {
+    bump: &'arena BumpArena,
+    blocks: IntMap<u64, Block<'arena>>,
+    blkmap: IntervalMap<u64, u64>,
+    graph: GraphMap<u64, FlowType, Directed, AHashState>,
+}
+
+impl<'arena> CFGraph<'arena> {
+    
+    pub fn new_with(bump: &'arena BumpArena) -> Self {
+        let blocks = IntMap::default();
+        let blkmap = IntervalMap::default();
+        let graph = GraphMap::default();
+        Self { bump, blocks, blkmap, graph }
+    }
+
+    #[instrument]
+    pub fn add_block(
+        &mut self,
+        range: Range<u64>,
+        insns: impl IntoIterator<Item=u64> + fmt::Debug,
+        successors: impl IntoIterator<Item=(FlowType, u64)> + fmt::Debug,
+        parent: Option<(FlowType, u64)>,
+    ) -> Result<(), Error> {
+        let block = Block::new_in(self.bump, range, insns, successors);
+        let address = block.address();
+        if self.blocks.contains_key(&address) {
+            return Err(Error::BlockAlreadyExists(block.address()));
+        }
+        
+        for (flowtype, target) in block.successors() {
+            if let Some(edge) = self.graph.add_edge(address, *target, *flowtype) {
+                warn!("edge already exists: {edge:?}({address:#x} -> {target:#x})");
+            }
+        }
+
+        if let Some((flowtype, parent)) = parent {
+            if let Some(edge) = self.graph.add_edge(parent, address, flowtype) {
+                warn!("edge already exists: {edge:?}({parent:#x} -> {address:#x})");
+            }
+        }
+
+        self.blkmap.insert(block.range().clone(), address);
+        self.blocks.insert(address, block);
+
+        Ok(())
+    }
+
+    #[instrument]
+    pub fn add_edge(
+        &mut self,
+        parent: u64,
+        child: u64,
+        flowtype: FlowType,
+    ) -> Result<(), Error> {
+        let Some((_, parent)) = self.blkmap.overlap(parent).next() else {
+            return Err(Error::BlockDoesNotExist(parent))
+        };
+        let Some((_, child)) = self.blkmap.overlap(child).next() else {
+            return Err(Error::BlockDoesNotExist(child))
+        };
+        if let Some(edge) = self.graph.add_edge(*parent, *child, flowtype) {
+            warn!("edge already exists: {edge:?}({parent:#x} -> {child:#x})");
+        }
+        Ok(())
+    }
+
+    pub fn get_block(&self, address: impl Into<u64>) -> Option<&Block<'arena>> {
+        let address = address.into();
+        let (_, blk_address) = self.blkmap.overlap(address).next()?;
+        self.blocks.get(blk_address)
+    }
+}
+
+impl<'arena> fmt::Debug for CFGraph<'arena> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "CFGraph [ {} blocks ; {} edges ]",
+            self.blocks.len(),
+            self.graph.edge_count(),
+        )
+    }
+}
---
+++ libcme/src/programdb/types.rs
@@ -0,0 +1,58 @@
+//! types.rs
+//! 
+//! smaller types and structs used in programdb module
+use std::ops::Range;
+use nohash::IntMap;
+pub use bumpalo::{
+    Bump as BumpArena,
+    collections::Vec as BumpVec,
+};
+
+use crate::types::*;
+
+pub(crate) type TranslationCache<'irb> = IntMap<u64, LiftResult<'irb>>;
+
+
+/// translation block
+#[derive(Debug, Clone)]
+pub struct Block<'arena> {
+    range: Range<u64>,
+    insns: BumpVec<'arena, u64>,
+    successors: BumpVec<'arena, (FlowType, u64)>,
+}
+
+impl<'arena> Block<'arena> {
+
+    pub fn new_in(
+        bump: &'arena BumpArena,
+        range: Range<u64>,
+        insns: impl IntoIterator<Item=u64>,
+        successors: impl IntoIterator<Item=(FlowType, u64)>,
+    ) -> Self {
+        let insns = BumpVec::from_iter_in(insns, bump);
+        let successors = BumpVec::from_iter_in(successors, bump);
+        Self { range, insns, successors }
+    }
+
+    pub fn address(&self) -> u64 {
+        self.range.start
+    }
+
+    pub fn size(&self) -> usize {
+        (self.range.end - self.range.start) as usize
+    }
+
+    pub fn range(&self) -> &Range<u64> {
+        &self.range
+    }
+
+    pub fn insns(&self) -> &[u64] {
+        &self.insns[..]
+    }
+
+    pub fn successors(&self) -> &[(FlowType, u64)] {
+        &self.successors[..]
+    }
+}
+
+
