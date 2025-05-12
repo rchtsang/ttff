782d7f1275550d8ff910b38b70e34f56a9dca351
rchtsang <rchtsang@ucdavis.edu>
2025-05-12 15:03:54 -0700

    change buffer length

--- ttff/examples/samples/uart-jump/main.c
+++ ttff/examples/samples/uart-jump/main.c
@@ -13,8 +13,8 @@
 
 
 #define MAX_TEST_DATA_BYTES (15U)   /**< max number of test bytes to be used for tx and rx. */
-#define UART_TX_BUF_SIZE    32     /**< UART TX buffer size. */
-#define UART_RX_BUF_SIZE    32     /**< UART RX buffer size. */
+#define UART_TX_BUF_SIZE    200     /**< UART TX buffer size. */
+#define UART_RX_BUF_SIZE    200     /**< UART RX buffer size. */
 
 
 int main(void)
